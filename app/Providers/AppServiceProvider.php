630fa336e9e7bb42155418a77254a050f91ee3be
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2025-07-08 09:34:36 +0700

    feat: init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Event;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
+            $event->extendSocialite('zitadel', \SocialiteProviders\Zitadel\Provider::class, );
+        });
+    }
+}
---
+++ resources/js/pages/auth/Login.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import AuthBase from '@/layouts/AuthLayout.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+import { LoaderCircle } from 'lucide-vue-next';
+
+defineProps<{
+    status?: string;
+    canResetPassword: boolean;
+}>();
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+    <AuthBase title="Log in to your account" description="Click the button below to log in with your SSO account.">
+
+        <Head title="Log in" />
+
+        <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit" class="flex flex-col gap-6">
+            <div class="grid gap-6">
+                <Button type="submit" class="mt-4 w-full" :tabindex="4" :disabled="form.processing">
+                    <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
+                    Log in with SSO
+                </Button>
+            </div>
+        </form>
+    </AuthBase>
+</template>
